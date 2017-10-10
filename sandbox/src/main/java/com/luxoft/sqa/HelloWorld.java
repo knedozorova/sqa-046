71d1ddae0a7a20156676342bd567db6d977ac79e
omsk18 <omsk18@ptc.local>
2017-10-10 14:06:01 +0600

    Create new module

---
+++ sandbox/src/main/java/com/luxoft/sqa/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.luxoft.sqa;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld{
+
+
+
+
+	public static void main(String[] args){
+
+		Point p1 = new Point(5, 1);
+		Point p2 = new Point(6, 6);
+
+			System.out.println(p1.distance(p2));
+//		System.out.println(test(p1,p2));
+//
+//	}
+//	public static double test(Point p1, Point p2) {
+//		return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+	}
+}
